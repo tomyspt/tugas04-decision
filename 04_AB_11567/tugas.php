7c764fbb81c4d08a7551d657e57aa5de0eb7fd15
tomy saputra <saputratomy325@gmail.com>
2017-08-28 23:55:57 +0700

    mengumpulkan tugas 04

---
+++ 04_AB_11567/tugas.php
@@ -0,0 +1,12 @@
+<?php
+	$nilai = "A-";
+	if($nilai == "A" || $nilai == "A-" || $nilai == "A/B" || $nilai == "B+" || $nilai == "B" || $nilai == "B-" || $niali == "B/C"){
+		echo "LULUS";
+	}elseif($nilai == "C+" || $nilai == "C" || $nilai == "C-" || $nilai == "C/D"){
+		echo "LULUS SEBAIKNYA DIULANG";
+	}elseif($nilai == "D+" || $nilai == "D"){
+		echo "LULUS & WAJIB DIULANG";
+	}elseif($nilai == "E"){
+	echo "TIDAK LULUS";
+	}
+?>
